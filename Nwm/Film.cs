e393bd72f730f01f31244146b274fbe0d8c97451
unknown <hamer.tv@outlook.com>
2022-12-08 11:34:00 +0100

    first commit

---
+++ Nwm/Film.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nwm
+{
+    public class Film
+    {
+        public string Nazwa { get; set; }
+
+        public int Ocena { get; set; }
+
+        public string Opis { get; set; }
+
+        public string Gatunek { get; set; }
+
+        public bool Oglodany { get; set; }
+
+        public Film(string nazwa, int ocena, string opis, string gatunek)
+        {
+            Nazwa = nazwa;
+            Ocena = ocena;
+            Opis = opis;
+            Gatunek = gatunek;
+            Oglodany = false;
+        }
+    }
+}
---
+++ Nwm/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nwm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public IEnumerable<string> KolekcjaStringow { get; set; }
+        public ObservableCollection<Film> WszystkieFilmy { get; set; }
+          public Film ZaznaczonyFilm { get; set; }
+
+        private void przygotujFilmy()
+        {
+            WszystkieFilmy = new ObservableCollection<Film>();
+
+            WszystkieFilmy.Add(new Film("Vaiana", 6, "Ciekawy film o szukaniu  celu w zyciu", "Animacja Wodna"));
+
+            WszystkieFilmy.Add(new Film("Vaiana2", 6, "Ciekawy film o szukaniu  celu w zyciu", "Animacja Wodna"));
+
+            WszystkieFilmy.Add(new Film("Toys Story", 6, "Ciekawy film o szukaniu  celu w zyciu", "Animacja Wodna"));
+
+            WszystkieFilmy.Add(new Film("Avatar 2", 6, "Nikt nie widzial nik tnie slyszal", "Sci Fi"));
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            KolekcjaStringow = " Ala ma kota a kot ma aleAla ma kota a kot ma aleAla ma kota a kot ma aleAla ma kota a kot ma ale ".Split(' ').ToList();
+            DataContext = this;
+            przygotujFilmy();
+            ZaznaczonyFilm = WszystkieFilmy[0];
+        }
+
+        
+        
+    }
+}
